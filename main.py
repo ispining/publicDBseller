fa2183ed5d8bbef9e7556c962ac8016073494f73
Anonymous <ilvi142014@gmail.com>
2022-12-05 00:18:13 +0200

    Development

---
+++ main.py
@@ -0,0 +1,29 @@
+from config import *
+import stg
+
+
+@bot.message_handler(commands=['start'])
+def start_msg(message):
+    chat_id = message.chat.id
+    if message.chat.type == "private":
+        pass
+
+
+@bot.message_handler(content_types=['text'])
+def text_msgs(message):
+    chat_id = message.chat.id
+    if message.chat.type == "private":
+        pass
+
+
+
+@bot.callback_query_handler(func=lambda m: True)
+def g_cals(call):
+    chat_id = call.message.chat.id
+    if call.message.chat.type == "private":
+        pass
+
+
+
+
+bot.polling()
