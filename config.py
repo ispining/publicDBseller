ab1791910642256ad6496876fd4d399b66958cff
Anonymous <ilvi142014@gmail.com>
2022-12-05 12:02:35 +0200

    Development

--- config.py
+++ config.py
@@ -16,36 +16,36 @@ kmarkup = tg.kmarkup
 btn = tg.btn
 
 def db_admin():
-    db = psycopg2.connect(user="postgres", password="armageddon", host="illyashost.ddns.net", port=5432)
-    db.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-    sql = db.cursor()
-    return db, sql
+    adb = psycopg2.connect(user="postgres", password="armageddon", host="illyashost.ddns.net", port=5432)
+    adb.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    asql = adb.cursor()
+    return adb, asql
 
 def db_user(username, password):
-    db = psycopg2.connect(database=username, user=username, password=password, host="illyashost.ddns.net", port=5432)
-    db.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-    sql = db.cursor()
-    return db, sql
+    adb = psycopg2.connect(database=username, user=username, password=password, host="illyashost.ddns.net", port=5432)
+    adb.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    asql = adb.cursor()
+    return adb, asql
 
-def db_close(db):
+def db_close(adb):
     try:
-        db.close()
+        adb.close()
     except:
         pass
 
 class DBcreator:
-    def __init__(self, role_name,password=None):
+    def __init__(self, role_name, password=None):
         self.role_name = role_name
         self.password = password
 
     def batabase_exists(self):
-        db, sql = db_admin()
+        adb, asql = db_admin()
         dbs = []
-        sql.execute("SELECT * FROM pg_catalog.pg_tables")
-        for i in sql.fetchall():
+        asql.execute("SELECT * FROM pg_catalog.pg_tables")
+        for i in asql.fetchall():
             if i[0] not in dbs:
                 dbs.append(i[0])
-        db_close(db)
+        db_close(adb)
         if self.role_name in dbs:
             return True
         else:
@@ -53,9 +53,9 @@ class DBcreator:
 
     # 1
     def create_role(self):
-        db, sql = db_admin()
+        adb, asql = db_admin()
         if self.role_name != None:
-            sql.execute(f"""CREATE ROLE {self.role_name} WITH 
+            asql.execute(f"""CREATE ROLE {self.role_name} WITH 
             LOGIN 
             NOSUPERUSER 
             INHERIT
@@ -63,40 +63,40 @@ class DBcreator:
             NOCREATEROLE
             NOREPLICATION
             PASSWORD '{self.password}'""")
-            db.commit()
+            adb.commit()
         else:
             print("[-] Cant create role without password")
-        db_close(db)
+        db_close(adb)
 
     # 2
     def create_db_for_role(self,
                            db_name: str,
                            connections: int = -1,
                            owner: str = "postgres"):
-        db, sql = db_admin()
+        adb, asql = db_admin()
         if connections == -1:
-            sql.execute(f"""CREATE DATABASE {db_name}
+            asql.execute(f"""CREATE DATABASE {db_name}
     WITH 
     OWNER = {owner}
     ENCODING = 'UTF8'
     """)
-            db.commit()
+            adb.commit()
         else:
-            sql.execute(f"""CREATE DATABASE {db_name}
+            asql.execute(f"""CREATE DATABASE {db_name}
     WITH 
     OWNER = {owner}
     ENCODING = 'UTF8'
     CONNECTION LIMIT = {connections}""")
-            db.commit()
+            adb.commit()
 
-        sql.execute(f"GRANT ALL ON DATABASE {db_name} TO {self.role_name}")
-        db.commit()
-        sql.execute(f"GRANT ALL ON DATABASE {db_name} TO {owner} WITH GRANT OPTION")
-        db.commit()
-        sql.execute(f"REVOKE ALL ON DATABASE {db_name} FROM public")
-        db.commit()
+        asql.execute(f"GRANT ALL ON DATABASE {db_name} TO {self.role_name}")
+        adb.commit()
+        asql.execute(f"GRANT ALL ON DATABASE {db_name} TO {owner} WITH GRANT OPTION")
+        adb.commit()
+        asql.execute(f"REVOKE ALL ON DATABASE {db_name} FROM public")
+        adb.commit()
 
-        db_close(db)
+        db_close(adb)
 
 
 
